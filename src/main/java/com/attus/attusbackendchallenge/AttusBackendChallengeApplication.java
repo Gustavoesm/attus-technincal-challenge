76a8c92ee3fcd17d35280911dea7921e1d456d83
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2024-05-03 15:04:23 -0300

    initial commit w/ Spring Initializr

---
+++ src/main/java/com/attus/attusbackendchallenge/AttusBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.attus.attusbackendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttusBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attus/attusbackendchallenge/AttusBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attus.attusbackendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttusBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
